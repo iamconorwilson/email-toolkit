0ba960b337e979f5ff774b4f96b5c3b88638e51e
Conor Wilson <conor.wilson@actionrocket.co>
2024-03-20 16:29:31 +0000

    Update index.js

--- bin/plugins/send/index.js
+++ bin/plugins/send/index.js
@@ -16,97 +16,97 @@ const command = (program) => {
 
 }
 
-const run = (options) => {
+const run = async (options) => {
     console.log(`[${chalk.magentaBright('email-pipeline')}] ${chalk.bold('Send')}`);
 
     let filePath = '';
 
     const config = loadConfig();
 
-    fs.readdir(process.cwd(), (err, files) => {
+    const files = fs.readdirSync(process.cwd(), (err, files) => {
         if (err) {
             console.error(`Error reading directory: ${err}`);
             return;
         }
+        return files;
+    }).filter(file => file.endsWith('.html'));
 
-        inquirer.prompt([
-            {
-                type: 'list',
-                name: 'file',
-                message: 'Select a file to send:',
-                choices: files,
-            },
-            {
-                type: 'input',
-                name: 'to',
-                message: 'Enter email address(es) to send to (comma separated):',
-                validate: (input) => {
-                    const valid = (email) => {
-                        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                        return re.test(String(email).toLowerCase());
-                    }
-                    let emails = input.split(',').map(e => e.trim());
-                    let invalidEmails = emails.filter(e => !valid(e));
-                    return invalidEmails.length === 0 ? true : `Invalid email(s): ${invalidEmails.join(', ')}`;
+    const answers = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'file',
+            message: 'Select a file to send:',
+            choices: files,
+        },
+        {
+            type: 'input',
+            name: 'to',
+            message: 'Enter email address(es) to send to (comma separated):',
+            validate: (input) => {
+                const valid = (email) => {
+                    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    return re.test(String(email).toLowerCase());
                 }
+                let emails = input.split(',').map(e => e.trim());
+                let invalidEmails = emails.filter(e => !valid(e));
+                return invalidEmails.length === 0 ? true : `Invalid email(s): ${invalidEmails.join(', ')}`;
             }
-        ]).then((answers) => {
+        }
+    ]);
 
-            filePath = `${process.cwd()}/${answers.file}`;
-            const { to } = answers;
+    filePath = `${process.cwd()}/${answers.file}`;
+    const { to } = answers;
 
-            //get file name from path
-            let fileName = filePath.split('/').pop();
+    //get file name from path
+    let fileName = filePath.split('/').pop();
 
-            console.log(`[${chalk.magentaBright('email-pipeline')}] Sending file: ${fileName}`);
+    console.log(`[${chalk.magentaBright('email-pipeline')}] Sending file: ${fileName}`);
 
-            //read secret from secrets.json in root of package
-            let credentials = config.send;
+    //read secret from secrets.json in root of package
+    let credentials = config.send;
 
-            //if no credentials, exit and warn user
-            if (!credentials) {
-                console.error(`[${chalk.magentaBright('email-pipeline')}] No credentials found in config file.`);
-                process.exit(1);
-            }
+    //if no credentials, exit and warn user
+    if (!credentials) {
+        console.error(`[${chalk.magentaBright('email-pipeline')}] No credentials found in config file.`);
+        process.exit(1);
+    }
 
-            // Set up nodemailer...
-            let transporter = nodemailer.createTransport({
-                'host': 'smtp.gmail.com',
-                'port': 465,
-                'secure': true,
-                'auth': {
-                    'user': credentials.email,
-                    'pass': credentials.password,
-                }
-            });
-
-            //read file to string
-            let fileContents = fs.readFileSync(filePath, 'utf8');
-
-            let mailOptions = {
-                from: credentials.test_email,
-                to: to,
-                subject: `Test Email: ${fileName}`,
-                text: 'This is a test email sent from Email Pipeline! If you are seeing this, change your client settings to view the HTML part of this email.',
-                html: fileContents,
-                headers: {
-                    References: randomUUID()
-                }
-            };
-
-
-            const spinner = ora(`[${chalk.magentaBright('email-pipeline')}] Sending email...`).start();
-
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    spinner.fail(`[${chalk.magentaBright('email-pipeline')}] Error sending email: ${error}`);
-                } else {
-                    spinner.succeed(`[${chalk.magentaBright('email-pipeline')}] Email sent: ${info.messageId}`);
-                    process.exit(0);
-                }
-            });
-        });
+    // Set up nodemailer...
+    let transporter = nodemailer.createTransport({
+        'host': 'smtp.gmail.com',
+        'port': 465,
+        'secure': true,
+        'auth': {
+            'user': credentials.email,
+            'pass': credentials.password,
+        }
     });
+
+    //read file to string
+    let fileContents = fs.readFileSync(filePath, 'utf8');
+
+    let mailOptions = {
+        from: credentials.test_email,
+        to: to,
+        subject: `Test Email: ${fileName}`,
+        text: 'This is a test email sent from Email Pipeline! If you are seeing this, change your client settings to view the HTML part of this email.',
+        html: fileContents,
+        headers: {
+            References: randomUUID()
+        }
+    };
+
+
+    const spinner = ora(`[${chalk.magentaBright('email-pipeline')}] Sending email...`).start();
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            spinner.fail(`[${chalk.magentaBright('email-pipeline')}] Error sending email: ${error}`);
+        } else {
+            spinner.succeed(`[${chalk.magentaBright('email-pipeline')}] Email sent: ${info.messageId}`);
+        }
+    });
+    process.exit(0);
 }
 
 export default command;
